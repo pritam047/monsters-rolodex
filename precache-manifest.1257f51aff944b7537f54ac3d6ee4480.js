bfb9d77e1073272fd222571f91bb1a7add8c5c28
pritam047 <pritamkar13@gmail.com>
2020-08-07 22:13:44 +0530

    Updates

---
+++ precache-manifest.1257f51aff944b7537f54ac3d6ee4480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180ea88efb854d056029f22f1ffaa0a2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "447a6e85c6767a1d0339",
+    "url": "/monsters-rolodex/static/css/main.9b7ab11f.chunk.css"
+  },
+  {
+    "revision": "86dcdc79c8fa6ca96a42",
+    "url": "/monsters-rolodex/static/js/2.1bbf21c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1bbf21c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447a6e85c6767a1d0339",
+    "url": "/monsters-rolodex/static/js/main.09a236e9.chunk.js"
+  },
+  {
+    "revision": "175dbf2b726cf515ab05",
+    "url": "/monsters-rolodex/static/js/runtime-main.6c9e3d0a.js"
+  }
+]);
